e9cbadeb23f7523f70a822d5f37e06426906e0de
Kontuch Suksawat <zeroxr20@gmail.com>
2025-01-24 18:18:25 +0700

    fix merge

--- client/src/admin/webDevReport.js
+++ client/src/admin/webDevReport.js
@@ -1,21 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { Table, Card } from "antd";
 import ax from "../conf/ax";
-
+import ShowReport from "./components/showReport";
 const AdminWebDevReport = () => {
-  const [student, setStudnet] = useState([]);
+  const [student, setStudent] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [transactionData, setTransactionData] = useState([]);
+
   useEffect(() => {
     const fetchStudent = async () => {
       try {
-        const userResult = await ax.get("/users?populate=role");
-        const userData = userResult.data;
-        console.log(userData);
+        // const userResult = await ax.get("/users?populate=role");
+        // const userData = userResult.data;
+        // console.log(userData);
 
-        const filterStudents = userData.filter(
-          (user) => user.role.type === "student"
+        // const filterStudents = userData.filter(
+        //   (user) => user.role.type === "student"
+        // );
+        // setStudnet(filterStudents);
+
+        const studentScore = await ax.get("/scores");
+        const studentData = studentScore.data.data;
+
+        const filterStudents = studentData.filter(
+          (user) => user.sID === "yqwnvt92d2zmu7sehv36akdf"
         );
-        setStudnet(filterStudents);
+        setStudent(filterStudents);
+        console.log("22222222222222222222222222", student);
+        console.log("-------------------", filterStudents);
+        console.log("+++++++++++++++++++++", student);
+        setTransactionData(
+          filterStudents.map((row) => ({
+            id: row.id,
+            key: row.id,
+            UID: row.UID,
+            Quiz1: row.Quiz1,
+            homeworkScore: row.homeworkScore,
+            MidtermScore: row.MidtermScore,
+            FinalScore: row.FinalScore,
+          }))
+        );
         setLoading(false);
       } catch (error) {
         console.error("Error fetching scores or user data:", error);
@@ -25,7 +49,6 @@ const AdminWebDevReport = () => {
 
     fetchStudent();
   }, []);
-  console.log("+++++++++++++++++++++++++", student);
 
   //   const mapstudent = student.map((item) => {
   //     console.log(item.username);
@@ -42,6 +65,7 @@ const AdminWebDevReport = () => {
           {student.map((item) => (
             <h2>{item.username}</h2>
           ))}
+          <ShowReport data={transactionData} />
         </div>
       )}
     </div>
